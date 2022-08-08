d2cb68a129b22b08430e71ccf438743d123f0785
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2022-08-08 11:32:07 +0300

    Updates

---
+++ precache-manifest.7a9da67c20d8ffefda9a8628e07ed4ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3a963cf722df40dc3cf",
+    "url": "/reactApp/static/css/main.099d00b3.chunk.css"
+  },
+  {
+    "revision": "c3a963cf722df40dc3cf",
+    "url": "/reactApp/static/js/main.c3a963cf.chunk.js"
+  },
+  {
+    "revision": "c0c76996cfebbc23532d",
+    "url": "/reactApp/static/js/1.c0c76996.chunk.js"
+  },
+  {
+    "revision": "ae19eedf6eab4da517e7",
+    "url": "/reactApp/static/js/runtime~main.ae19eedf.js"
+  },
+  {
+    "revision": "3188e1cfc2bb7dcb5a428e340b037c3b",
+    "url": "/reactApp/index.html"
+  }
+];
